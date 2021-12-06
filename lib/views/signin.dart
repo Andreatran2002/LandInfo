ecbc0a527e5b87097a72bd735499e202343a4b99
phuongvy <phuongvy.maicogroup@gmail.com>
2021-12-06 12:12:20 +0700

    update-sign-up-with-email

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -4,7 +4,9 @@ import 'package:flutter/rendering.dart';
 import 'package:tineviland/Widgets/widget.dart';
 import 'signup.dart';
 import 'forgetpassword.dart';
+import 'home.dart';
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+
 class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
 
@@ -13,7 +15,7 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
-  firebase_auth.FirebaseAuth firebaseAuth =  firebase_auth.FirebaseAuth.instance;
+  firebase_auth.FirebaseAuth firebaseAuth = firebase_auth.FirebaseAuth.instance;
   @override
   void initState() {
     super.initState();
@@ -39,14 +41,14 @@ class _SignInState extends State<SignIn> {
   bool circular = false;
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar : AppBar(),
+        appBar: AppBar(),
         body: Form(
             key: _formKey,
             child: ListView(
               padding: EdgeInsets.symmetric(horizontal: 24),
               children: <Widget>[
                 Column(children: <Widget>[
-                  const SizedBox(height: 120.0),
+                  const SizedBox(height: 70.0),
                   Image.asset(
                     'assets/Logo.png',
                     height: 150,
@@ -57,29 +59,26 @@ class _SignInState extends State<SignIn> {
                     color: Colors.black,
                   ),
                   controller: _emailController,
-                  keyboardType: TextInputType.phone,
+                  keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
-                    labelText: "Số điện thoại",
+                    labelText: "Email",
                     labelStyle: TextStyle(
                       color: _emailFocusNode.hasFocus
                           ? Theme.of(context).colorScheme.secondary
                           : _unfocusedColor,
                     ),
                   ),
-
                   validator: (value) {
-                    if (value == "") {
-                      return 'Vui lòng không được để trống số điện thoại!';
-                    }
-
-                    else if (!RegExp(r'(^(?:[+0]9)?[0-9]{10}$)').hasMatch(value!) || value.length > 10)
-                      return "Vui lòng nhập số điện thoại hợp lệ!";
-
-                    return null;
+                    if (!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                            .hasMatch(value!) ||
+                        value == null)
+                      return 'Vui lòng nhập email đúng định dạng example@gmail.com';
+                    else
+                      return null;
                   },
                   focusNode: _emailFocusNode,
                 ),
-                const SizedBox(height: 20.0),
+                const SizedBox(height: 10.0),
                 TextFormField(
                   style: TextStyle(
                     color: Colors.black,
@@ -89,8 +88,9 @@ class _SignInState extends State<SignIn> {
                   decoration: InputDecoration(
                       labelText: "Mật khẩu",
                       suffixIcon: IconButton(
-                          icon: Icon(
-                              _isObscurePassword ? Icons.visibility : Icons.visibility_off),
+                          icon: Icon(_isObscurePassword
+                              ? Icons.visibility
+                              : Icons.visibility_off),
                           onPressed: () {
                             setState(() {
                               _isObscurePassword = !_isObscurePassword;
@@ -103,7 +103,7 @@ class _SignInState extends State<SignIn> {
                       )),
                   focusNode: _passwordFocusNode,
                   validator: (value) {
-                    if (value=="") {
+                    if (value == "") {
                       return 'Vui lòng không được bỏ trống mật khẩu';
                     } else {
                       if (value!.length < 8) {
@@ -115,123 +115,89 @@ class _SignInState extends State<SignIn> {
                   },
                   obscureText: _isObscurePassword,
                 ),
-                const SizedBox(height: 20.0),
+                const SizedBox(height: 10.0),
                 Container(
                   alignment: Alignment.centerRight,
-
                   child: Container(
                     child: TextButton(
-                      child: Text(
-                      'Quên mật khẩu?',
-                      style: simpleTextFieltStyle()
-                      ),
-                      onPressed:(){
+                      child:
+                          Text('Quên mật khẩu?', style: simpleTextFieltStyle()),
+                      onPressed: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => ForgetPassword()),
+                          MaterialPageRoute(
+                              builder: (context) => ForgetPassword()),
                         );
-                      } ,
-
+                      },
                     ),
                   ),
                 ),
-
-                ButtonBar(
-                  alignment: MainAxisAlignment.spaceAround,
-                  buttonMinWidth: 300,
-                  children: <Widget>[
-                    ElevatedButton(
-                      child: circular ? CircularProgressIndicator( color: Colors.white,):  const Text('Đăng nhập' ,
-                          style:TextStyle(
+                ElevatedButton(
+                  child: circular
+                      ? CircularProgressIndicator(
+                          color: Colors.white,
+                        )
+                      : const Text('Đăng nhập',
+                          style: TextStyle(
+                            height: 1.5,
+                            fontSize: 17,
                             color: Colors.white,
-                          )
-
+                          )),
+                  style: ButtonStyle(
+                    elevation: MaterialStateProperty.all(8.0),
+                    shape: MaterialStateProperty.all(
+                      const BeveledRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(3.0)),
                       ),
-                      style: ButtonStyle(
-                        elevation: MaterialStateProperty.all(8.0),
-
-                        shape: MaterialStateProperty.all(
-                          const BeveledRectangleBorder(
-                            borderRadius:
-                            BorderRadius.all(Radius.circular(3.0)),
-                          ),
-                        ),
-                      ),
-                      onPressed: () async {
-                        setState(() {
-                          circular = true;
-                        });
-                        try {
-                          if (_formKey.currentState!.validate()) {
-                            firebase_auth.UserCredential userCredential = await firebaseAuth
-                                .signInWithEmailAndPassword(
+                    ),
+                  ),
+                  onPressed: () async {
+                    setState(() {
+                      circular = true;
+                    });
+                    try {
+                      if (_formKey.currentState!.validate()) {
+                        firebase_auth.UserCredential userCredential =
+                            await firebaseAuth.signInWithEmailAndPassword(
                                 email: _emailController.text,
                                 password: _passwordController.text);
-                            print(userCredential.user!.email);
-                            setState(() {
-                              circular = false;
-                            });
-                            Navigator.pushAndRemoveUntil(context,
-                                MaterialPageRoute(builder: (builder)=>SignIn()),
-                                    (route) => false);
-                          }
-                        } catch (e){
-                          final snackbar = SnackBar(content : Text(e.toString()));
-                          ScaffoldMessenger.of(context).showSnackBar(snackbar);
-                          setState(() {
-                            circular = false;
-                          });
-                        }
-                      },
-                    ),
-                    TextButton(
-                      child: const Text('Quay lại'),
-                      style: ButtonStyle(
-                        foregroundColor: MaterialStateProperty.all(
-                            Theme.of(context).colorScheme.secondary),
-                        shape: MaterialStateProperty.all(
-                          const BeveledRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(7.0)),
-                          ),
-                        ),
-                      ),
-                      onPressed: () {
-                        _emailController.clear();
-                        _passwordController.clear();
-                      },
-                    ),
-
-                  ],
+                        print(userCredential.user!.email);
+                        setState(() {
+                          circular = false;
+                        });
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(builder: (builder) => Home()),
+                            (route) => false);
+                      }
+                    } catch (e) {
+                      final snackbar = SnackBar(content: Text(e.toString()));
+                      ScaffoldMessenger.of(context).showSnackBar(snackbar);
+                      setState(() {
+                        circular = false;
+                      });
+                    }
+                  },
                 ),
-                Row(
-                  mainAxisAlignment : MainAxisAlignment.center ,
-                  children: [
-                    Text(
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Text(
                     'Chưa có tài khoản !',
                     style: simpleTextFieltStyle(),
                   ),
-
-                    TextButton(
-                        child: const Text("Đăng ký ngay",
-                            style : TextStyle(
-                                  fontSize: 14,
-                                decoration: TextDecoration.underline
-                            )),
-
-                      onPressed:()=>{
+                  TextButton(
+                    child: const Text("Đăng ký ngay",
+                        style: TextStyle(
+                            fontSize: 14,
+                            decoration: TextDecoration.underline)),
+                    onPressed: () => {
                       Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SignUp()),
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUp()),
                       )
-                      },
-
-                    )
-                  ]
-                ),
-
+                    },
+                  )
+                ]),
               ],
             )));
   }
-
 }
