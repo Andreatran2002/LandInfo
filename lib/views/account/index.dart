794e0f6b75b020721c986b7cdbb604b5732287bf
Phạm Nhật Tiến <83960491+1wku@users.noreply.github.com>
2021-12-10 21:35:53 +0700

    add navigation

--- lib/views/account/index.dart
+++ lib/views/account/index.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class account extends StatelessWidget {
-  const account({Key? key}) : super(key: key);
+class Account extends StatelessWidget {
+  const Account({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:carousel_slider/carousel_slider.dart';
-import '../constants.dart';
-import 'home/banner_slider.dart';
-import 'home/news_slider.dart';
+import './home/index.dart';
+import './news/index.dart';
+import './account/index.dart';
+import './posts/add_post.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,6 +14,13 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   late ScrollController _scrollController;
   late double _scrollPosition;
+  final screens = [
+    const Home(),
+    News(),
+    AddPost(),
+    Account(),
+    // const
+  ];
   int currentPageIndex = 0;
 
   _scrollListener() {
@@ -41,18 +47,24 @@ class _HomeState extends State<Home> {
         type: BottomNavigationBarType.fixed,
         currentIndex: currentPageIndex,
         onTap: (index) {
-          setState(() {
-            currentPageIndex = index;
-          });
+          if (index != currentPageIndex) {
+            // Navigator.push(context,
+            //     MaterialPageRoute(builder: (context) => screens[index]));
+            setState(() {
+              currentPageIndex = index;
+            });
+          }
         },
         selectedItemColor: const Color(0xff358F38),
         unselectedItemColor: const Color(0xffC4C4C4),
         iconSize: 30,
         selectedLabelStyle: const TextStyle(
           fontFamily: "Montserrat",
+          fontWeight: FontWeight.w600,
         ),
         unselectedLabelStyle: const TextStyle(
           fontFamily: "Montserrat",
+          fontWeight: FontWeight.w600,
         ),
         items: const [
           BottomNavigationBarItem(
@@ -74,92 +86,13 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        controller: _scrollController,
-        child: Stack(
-          children: <Widget>[
-            Container(
-              height: size.height * 0.4,
-              decoration: const BoxDecoration(
-                color: kGreenLightColor,
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(10),
-                  bottomLeft: Radius.circular(10),
-                ),
-              ),
-            ),
-            SafeArea(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Opacity(
-                    opacity: ((90 - _scrollPosition) / 100 >= 0)
-                        ? (90 - _scrollPosition) / 100
-                        : 0,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 20,
-                      ),
-                      child: Row(
-                        // key: headingKey,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Image.asset(
-                            "assets/images/mainLogo.png",
-                            height: 90,
-                          ),
-                          Text(
-                            "Chào, Vy",
-                            style:
-                                Theme.of(context).textTheme.headline4?.copyWith(
-                                      fontWeight: FontWeight.w700,
-                                      fontFamily: "Montserrat",
-                                    ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 14,
-                        vertical: 5,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: TextField(
-                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                              fontWeight: FontWeight.w600,
-                              fontFamily: "Montserrat",
-                              fontSize: 16,
-                            ),
-                        decoration: InputDecoration(
-                          hintText: "Tìm kiếm...",
-                          icon: SvgPicture.asset("assets/icons/search.svg"),
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 15),
-                    child: SliderForBanner(),
-                  ),
-                  const SliderForNews(title: 'Mua bán'),
-                  const SizedBox(height: 10),
-                  const SliderForNews(title: 'Tin tức'),
-                  // SliderForNews(),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: currentPageIndex == 0
+          ? HomePage(
+              scrollController: _scrollController,
+              size: size,
+              scrollPosition: _scrollPosition,
+            )
+          : screens[currentPageIndex],
     );
   }
 }
