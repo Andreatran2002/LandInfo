d32d49931eb6854d19b398aec6b2723d461c0c82
phuongvy <phuongvy.maicogroup@gmail.com>
2021-12-21 17:00:58 +0700

    update detail land planning

--- lib/views/land_use_planning/detail_land_planning.dart
+++ lib/views/land_use_planning/detail_land_planning.dart
@@ -1,5 +1,11 @@
+import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui' ;
+import 'Dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:image/image.dart' as img;
 import 'package:provider/provider.dart';
 import 'package:tineviland/blocs/application_bloc.dart';
 class DetailLandPlanning extends StatefulWidget {
@@ -15,12 +21,15 @@ class _DetailLandPlanningState extends State<DetailLandPlanning> {
   void initState() {
     super.initState();
     markers = Set.from([]);
-    BitmapDescriptor.fromAssetImage(
-        ImageConfiguration(size: Size(48, 48)), 'assets/images/binhthuanland.png')
-        .then((onValue) {
-      myIcon = onValue;
-    });
+
+    // final Bitmap bitmap = Bitmap.fromAssetImage(image);
+    // BitmapDescriptor.fromAssetImage(
+    //     const ImageConfiguration(devicePixelRatio: 2.5,size: Size(10, 10)), 'assets/images/binhthuanland.png')
+    //     .then((onValue) {
+    //   myIcon = onValue;
+    // });
   }
+
   late GoogleMapController mapController;
   BitmapDescriptor? myIcon ;
   late Set<Marker> markers;
@@ -42,16 +51,21 @@ class _DetailLandPlanningState extends State<DetailLandPlanning> {
               myLocationButtonEnabled: true,
               initialCameraPosition: const CameraPosition(
                 target: LatLng(10.856809388642066, 106.77465589400319),
+                zoom: 14,
               ),
               markers: markers,
-              onLongPress:  (pos) {
+              onLongPress:  (pos) async {
                 print(pos);
                 print("he");
-                Marker f =
-                Marker(markerId: MarkerId(pos.toString()), icon: myIcon!, position: pos,
-                    onTap: (){});
+                final icon = await getBitmapDescriptorFromAssetBytes('assets/images/binhthuanland.png', 1000);
+
+                MediaQueryData mediaQueryData = MediaQuery.of(context);
+                ImageConfiguration imageConfig = ImageConfiguration(devicePixelRatio: mediaQueryData.devicePixelRatio);
+                final Uint8List markerIcon = await getBytesFromAsset('assets/images/binhthuanland.png',1000 );
+                final Marker marker = Marker(icon: icon, position: pos,markerId: MarkerId(pos.toString()));
+
                 setState(() {
-                  markers.add(f);
+                  markers.add(marker);
                 });
 
               },
@@ -63,5 +77,16 @@ class _DetailLandPlanningState extends State<DetailLandPlanning> {
         )
     );
 
+  }
+  Future<Uint8List> getBytesFromAsset(String path, int width) async {
+    ByteData data = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(), targetWidth: width);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!.buffer.asUint8List();
+  }
+
+  Future<BitmapDescriptor> getBitmapDescriptorFromAssetBytes(String path, int width) async {
+    final Uint8List imageData = await getBytesFromAsset(path, width);
+    return BitmapDescriptor.fromBytes(imageData);
   }
 }
